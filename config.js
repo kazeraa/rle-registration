ff4fc48a3af44a179e082533ccfeda6120b53935
kazeraa <tiwing20@gmail.com>
2026-06-29 09:28:37 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   logoUrl: "https://files.catbox.moe/n9y5ks.jpg",
 
   secretKey: "adminrle26",
-  waGroupLink: "https://chat.whatsapp.com/IH5ROjY8vP6LRtgEzCwjoO?s=cl&p=a&ilr=0",
+  waGroupLink: "https://chat.whatsapp.com/FEJmHxbbL0cFAqxdTgeqZ3?s=cl&p=a&ilr=0",
 
   supabaseUrl: "https://lyrbfjrdtotjdcqcyjar.supabase.co",
   supabaseAnonKey: "sb_publishable_oi8r8cTDseyE2rrKL2fFnw_AAkr9HUb"
